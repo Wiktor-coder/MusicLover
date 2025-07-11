2826d57e5410efbad05187a7a0958cbaac1279f0
Wiktor Chumakov <Apostal333@gmail.com>
2025-07-11 22:50:20 +0300

    added file MusicLover

---
+++ src/main/kotlin/MusicLover.kt
@@ -0,0 +1,32 @@
+fun main() {
+
+    var ordinary: Boolean = true
+    val constant: Boolean = false
+    val purchaseAmount = 10_001
+
+    if (constant) {
+        if (purchaseAmount <= 1_000) {
+            println("Сумма оплаты со скидкой: ${purchaseAmount - (purchaseAmount * 0.01).toInt()}")
+        } else if (purchaseAmount >= 1_001 && purchaseAmount <= 10_000) {
+            println("Сумма оплаты со скидкой: ${purchaseAmount - (purchaseAmount * 0.01).toInt() - 100}")
+        } else if (purchaseAmount >= 10_001) {
+            println("Сумма оплаты со скидкой: ${purchaseAmount - (purchaseAmount * 0.06).toInt()}")
+        }
+        ordinary = false
+    }
+    if (ordinary) {
+        if (purchaseAmount <= 1_000) {
+            println("Сумма оплаты со скидкой: $purchaseAmount")
+        } else if (purchaseAmount >= 1_001 && purchaseAmount <= 10_000) {
+            println("Сумма оплаты со скидкой: ${purchaseAmount - 100}")
+        } else if (purchaseAmount >= 10_001) {
+            println("Сумма оплаты со скидкой: ${purchaseAmount - (purchaseAmount * 0.05).toInt()}")
+        }
+    }
+}
+
+/***
+ * от 0 до 1 000 рублей — скидка не предоставляется
+ * от 1 001 до 10 000 рублей — скидка составит 100 рублей (как в лекции)
+ * от 10 001 рубля и выше — скидка составит 5% от суммы.
+ */
