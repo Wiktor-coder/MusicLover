e3dfd4d92d7fd931d42db04f33f448ed82cffb74
Wiktor Chumakov <Apostal333@gmail.com>
2025-07-11 22:58:34 +0300

    added in file MusicLover discount

--- src/main/kotlin/MusicLover.kt
+++ src/main/kotlin/MusicLover.kt
@@ -2,13 +2,14 @@ fun main() {
 
     var ordinary: Boolean = true
     val constant: Boolean = false
+    val discount = 100
     val purchaseAmount = 10_001
 
     if (constant) {
         if (purchaseAmount <= 1_000) {
             println("Сумма оплаты со скидкой: ${purchaseAmount - (purchaseAmount * 0.01).toInt()}")
         } else if (purchaseAmount >= 1_001 && purchaseAmount <= 10_000) {
-            println("Сумма оплаты со скидкой: ${purchaseAmount - (purchaseAmount * 0.01).toInt() - 100}")
+            println("Сумма оплаты со скидкой: ${purchaseAmount - (purchaseAmount * 0.01).toInt() - discount}")
         } else if (purchaseAmount >= 10_001) {
             println("Сумма оплаты со скидкой: ${purchaseAmount - (purchaseAmount * 0.06).toInt()}")
         }
@@ -18,7 +19,7 @@ fun main() {
         if (purchaseAmount <= 1_000) {
             println("Сумма оплаты со скидкой: $purchaseAmount")
         } else if (purchaseAmount >= 1_001 && purchaseAmount <= 10_000) {
-            println("Сумма оплаты со скидкой: ${purchaseAmount - 100}")
+            println("Сумма оплаты со скидкой: ${purchaseAmount - discount}")
         } else if (purchaseAmount >= 10_001) {
             println("Сумма оплаты со скидкой: ${purchaseAmount - (purchaseAmount * 0.05).toInt()}")
         }
