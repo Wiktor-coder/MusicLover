d96a69c5790f1d80aac0210c5e85120ffc72f726
Wiktor Chumakov <Apostal333@gmail.com>
2025-07-12 22:16:21 +0300

    fixed file MusicLover

--- src/main/kotlin/MusicLover.kt
+++ src/main/kotlin/MusicLover.kt
@@ -1,27 +1,24 @@
 fun main() {
 
-    var ordinary: Boolean = true
-    val constant: Boolean = false
+    val constant: Boolean = true
     val discount = 100
     val purchaseAmount = 10_001
 
-    if (constant) {
-        if (purchaseAmount <= 1_000) {
-            println("Сумма оплаты со скидкой: ${purchaseAmount - (purchaseAmount * 0.01).toInt()}")
-        } else if (purchaseAmount >= 1_001 && purchaseAmount <= 10_000) {
-            println("Сумма оплаты со скидкой: ${purchaseAmount - (purchaseAmount * 0.01).toInt() - discount}")
-        } else if (purchaseAmount >= 10_001) {
-            println("Сумма оплаты со скидкой: ${purchaseAmount - (purchaseAmount * 0.06).toInt()}")
-        }
-        ordinary = false
+//    Сначала применяем стандартную скидку
+    val standardDiscount = if (purchaseAmount <= 1_000) {
+        purchaseAmount
+    } else if (purchaseAmount <= 10_000) {
+        purchaseAmount - discount
+    } else {
+        purchaseAmount - (purchaseAmount * 0.05).toInt()
     }
-    if (ordinary) {
-        if (purchaseAmount <= 1_000) {
-            println("Сумма оплаты со скидкой: $purchaseAmount")
-        } else if (purchaseAmount >= 1_001 && purchaseAmount <= 10_000) {
-            println("Сумма оплаты со скидкой: ${purchaseAmount - discount}")
-        } else if (purchaseAmount >= 10_001) {
-            println("Сумма оплаты со скидкой: ${purchaseAmount - (purchaseAmount * 0.05).toInt()}")
-        }
+
+//    Затем применяем скидку постоянного покупателя
+    val finalAmount = if (constant) {
+        standardDiscount - (standardDiscount * 0.01).toInt()
+    } else {
+        standardDiscount
     }
+
+    println("Сумма оплаты со скидкой: $finalAmount рублей")
 }
